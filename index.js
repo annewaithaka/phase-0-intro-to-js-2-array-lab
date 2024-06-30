f9c2a7b23311b8d39c44eb2204137ea43f94f88c
annewaithaka <annewaithaka68@gmail.com>
2024-06-30 21:12:56 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Append at the end of the array
+let destructivelyAppendCat = (name) => {
+  cats.push(name);
+};
+
+// Prepend at the beginning of the array
+const destructivelyPrependCat = (name) => {
+  cats.unshift(name);
+};
+
+// Remove the last cat at the array
+let destructivelyRemoveLastCat = () => {
+  cats.pop();
+};
+
+// Remove the first cat from the cats array
+let destructivelyRemoveFirstCat = () => {
+  cats.shift();
+};
+
+// Append a cat to the cats array and return a new array
+let appendCat = (name) => {
+  return [...cats, name];
+};
+
+// Prepend a cat to the cats array and return a new array
+let prependCat = (name) => {
+  return [name, ...cats];
+};
+
+// Remove the last cat and return a new array
+let removeLastCat = () => {
+  return cats.slice(0, -1);
+};
+
+// Remove the first cat and return a new array
+let removeFirstCat = () => {
+  return cats.slice(1);
+};
